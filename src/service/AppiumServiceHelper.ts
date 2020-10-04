7c906b214bb1022859921eb3cd3951e419e79ca2
xiang19890319@gmail.com <xiang19890319@gmail.com>
2020-10-05 07:43:03 +0800

    create new session when the old one is dead

--- src/service/AppiumServiceHelper.ts
+++ src/service/AppiumServiceHelper.ts
@@ -5,25 +5,36 @@ export class AppiumServiceHelper {
 
     private sessionCache: Map<string, any> = new Map();
 
+    private async createAndSave(deviceName: string, platform: string, platformVersion: string):Promise<any> {
+        const opts = {
+            path: '/wd/hub',
+            port: 14567,
+            capabilities: {
+                platformName: platform,
+                platformVersion: platformVersion,
+                automationName: 'UiAutomator2',
+                "deviceName": deviceName,
+                "noReset": true
+            }
+        };
+        let client = await wdio.remote(opts);
+        this.sessionCache.set(deviceName, client);
+        return client;
+    }
+
     public async getSession(deviceName: string, platform: string, platformVersion: string): Promise<any> {
 
         if (this.sessionCache.get(deviceName)) {
-            return this.sessionCache.get(deviceName);
+            let cacheSession = this.sessionCache.get(deviceName);
+            try {
+                let status = await cacheSession.status();
+            } catch (error) {
+                //the session is dead, create new session
+                return await this.createAndSave(deviceName, platform, platformVersion);
+            }
+            return cacheSession;
         } else {
-            const opts = {
-                path: '/wd/hub',
-                port: 14567,
-                capabilities: {
-                    platformName: platform,
-                    platformVersion: platformVersion,
-                    automationName: 'UiAutomator2',
-                    "deviceName": deviceName,
-                    "noReset": true
-                }
-            };
-            let client = await wdio.remote(opts);
-            this.sessionCache.set(deviceName, client);
-            return client;
+            return await this.createAndSave(deviceName, platform, platformVersion);
         }
 
     }
